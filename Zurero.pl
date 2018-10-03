43a67a2737d2eb9670047e34fc1645ab51bcb672
Eduardo Silva <eduardo__lps@hotmail.com>
2018-10-03 11:08:24 +0100

    Added initial file.

---
+++ Zurero.pl
@@ -0,0 +1,16 @@
+/* table = [['A19', 'B19', 'C19', 'D19', 'E19', 'F19', 'G19', 'H19', 'I19', 'D19'J], ['A18']]. */
+
+table = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []].    
+
+
+getPiece(Line, Column , Table, Piece) :-
+    getLine(Line, Table, ActualLine),
+    getColumn(Column, ActualLine, Piece).
+
+getLine(1, [Line | _ ], Line).
+getLine(N, [ _ | Remainder], Line) :-
+    N > 1,
+    Previous is N - 1,
+    getLine(Previous, Remainder, Line).
+
+
