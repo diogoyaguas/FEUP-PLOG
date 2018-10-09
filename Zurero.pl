cac5d275bf125c7ea06be74e11aa48b133c72f56
Eduardo Silva <eduardo__lps@hotmail.com>
2018-10-09 22:00:34 +0100

    Added first version of replaceItemInColumn function

--- Zurero.pl
+++ Zurero.pl
@@ -1,26 +1,47 @@
 table([
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
-[empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty,empty]]).    
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,b,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.],
+[.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.]]).  
 
+line([.,.,b]).
+
+black('Player 1').
+black('B').
+white('Player 2').
+white('W').
+
+viewTab([]).
+viewTab([H|T]) :-
+    printList(H),
+    viewTab(T).
+
+printList([]) :-
+    nl.
+printList([H|T]) :-
+    write(H),
+    write('|'),
+    printList(T).
+
+display_game(Board) :- table(Board), viewTab(Board).
 
 getPiece(Line, Column , Table, Piece) :-
+    table(Table),
     getLine(Line, Table, ActualLine),
     getColumn(Column, ActualLine, Piece).
 
@@ -35,5 +56,20 @@ getColumn(N, [ _ | Remainder], Piece) :-
     N > 1,
     Previous is N - 1,
     getColumn(Previous, Remainder, Piece).
+
+replacePiece(Column, List, NewElement, NewLine) :-
+    replacePieceInColumn(Column, List, NewElement, NewLine).
+
+replacePieceInColumn(1, [ _ | Remainder ], NewElement, NewLine) :-
+    append(NewLine, [NewElement], NewNewLine),
+    append(NewNewLine, Remainder, NewLine3),
+    printList(NewLine3).
+
+replacePieceInColumn(Column, [ Head | Remainder], NewElement, NewLine) :-
+    Column > 1,
+    Previous is Column - 1,
+    append(NewLine, [Head], NewNewLine),
+    replacePieceInColumn(Previous, Remainder, NewElement, NewNewLine).
+    
 
 
