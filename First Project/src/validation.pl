344d0707526134897cadcf6d51771c24883eec03
Eduardo Silva <eduardo__lps@hotmail.com>
2018-11-17 00:06:55 +0000

    Fixed bug in validation

--- First Project/src/validation.pl
+++ First Project/src/validation.pl
@@ -8,49 +8,38 @@ get_moves([ ], Moves, _, _, _, Moves).
 
 get_moves([Line | RestOfBoard], Moves, Index, Symbol, Player, ListOfMoves) :-
         has_pieces(Line), 
-        check_last_element_and_add_play(Line, Moves, Index, Symbol, Player, PlayOne, NewMoves),
-        check_first_element_and_add_play(Line, NewMoves, Index, Symbol, Player, PlayOne, PlayTwo, NewMoves2),
+        check_last_element_and_add_play(Line, Moves, Index, Symbol, Player, NewMoves),
+        check_first_element_and_add_play(Line, NewMoves, Index, Symbol, Player, NewMoves2),
         NextIndex is (Index + 1),
-        (
-            (PlayTwo \= [], get_moves(RestOfBoard, NewMoves2, NextIndex, Symbol, Player, ListOfMoves));
-
-            get_moves(RestOfBoard, NewMoves, NextIndex, Symbol, Player, ListOfMoves)
-        ).
+        get_moves(RestOfBoard, NewMoves2, NextIndex, Symbol, Player, ListOfMoves).
     
 get_moves([_ | RestOfBoard], Moves, Index, Symbol, Player, ListOfMoves) :-
     NextIndex is (Index + 1),
     get_moves(RestOfBoard, Moves, NextIndex, Symbol, Player, ListOfMoves).
 
-check_first_element_and_add_play(Line, Moves, Index, Symbol, Player, FirstPlay, NewPlay, NewMoves) :-
+check_first_element_and_add_play(Line, Moves, Index, Symbol, Player, NewMoves) :-
     check_first_element(Line),
-    (Symbol = 'L', SecondDirection = 'R'; Symbol = 'C', SecondDirection = 'U'),
+    (Symbol = 'L', SecondDirection = 'R'; Symbol = 'C', SecondDirection = 'D'),
     NewPlay = [Symbol, Index, SecondDirection, Player],
-    add_play(FirstPlay, NewPlay, Moves, NewMoves).
+    append([NewPlay], Moves, NewMoves).
 
-check_first_element_and_add_play(_, _, _, _, _, _, NewPlay, _) :-
-    NewPlay = [].
-
-add_play(FirstPlay, NewPlay, OldMoves, NewMoves) :-
-    FirstPlay \= [], !,
-    append([NewPlay], OldMoves, NewMoves).
-
-add_play(_, NewPlay, OldMoves, NewMoves) :-
-    append([NewPlay], OldMoves, NewMoves).
+check_first_element_and_add_play(_, Moves, _, _, _, NewMoves) :-
+    NewMoves = Moves.
 
 check_first_element([FirstElement | _]) :-
-    FirstElement = empty.
+    FirstElement = empty, !.
 
 check_first_element([_ | [SecondElement | _]]) :-
-    SecondElement = empty.
+   SecondElement = empty.
 
-check_last_element_and_add_play(Line, Moves, Index, Symbol, Player, NewPlay, NewMoves) :-
+check_last_element_and_add_play(Line, Moves, Index, Symbol, Player, NewMoves) :-
     check_last_element(Line), !,
-    (Symbol = 'L', FirstDirection = 'L'; Symbol = 'C', FirstDirection = 'D'),
+    (Symbol = 'L', FirstDirection = 'L'; Symbol = 'C', FirstDirection = 'U'),
     NewPlay = [Symbol, Index, FirstDirection, Player],
     append([NewPlay], Moves, NewMoves).
 
-check_last_element_and_add_play(_, _, _, _, _, NewPlay, _) :-
-    NewPlay = [].
+check_last_element_and_add_play(_, Moves, _, _, _, NewMoves) :-
+    NewMoves = Moves.
 
 check_last_element(Line) :-
     get_last_element(Line, LastElement),
