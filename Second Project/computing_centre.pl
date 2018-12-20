a4195b8c33cd0fba4c7d0fd14af2867d40a6e52f
Eduardo Silva <eduardo__lps@hotmail.com>
2018-12-20 23:03:10 +0000

    Time Restriction Added

--- Second Project/computing_centre.pl
+++ Second Project/computing_centre.pl
@@ -34,29 +34,12 @@ schedule(ServerList, ServerNo, TaskList, TaskNo, StartTimes, EndTimes, MachineId
     length(EndTimes, TaskNo),
     domain(StartTimes, 0, 86400), %Check what domain is best (hours vs minutes vs ?)
     domain(EndTimes, 0, 86400),
-    %Tasks
-    length(MachineIds, TaskNo),
-    domain(MachineIds, 1, ServerNo),
-    length(Tasks1, TaskNo),
-    length(Tasks2, TaskNo),
-    length(Tasks3, TaskNo),
-    length(Tasks4, TaskNo),
-    create_prolog_tasks(TaskList, StartTimes, EndTimes, MachineIds, Tasks1, Tasks2, Tasks3, Tasks4),
-    /*
-    write('----- Tasks 1 ----- '), nl,
-    print_list(Tasks1), nl,
-    write('----- Tasks 2 ----- '), nl,
-    print_list(Tasks2), nl,
-    write('----- Tasks 3 ----- '), nl,
-    print_list(Tasks3), nl,
-    write('----- Tasks 4 ----- '), nl,
-    print_list(Tasks4), nl, */
     %Machines
     length(Machines1, ServerNo),
     length(Machines2, ServerNo),
     length(Machines3, ServerNo),
     length(Machines4, ServerNo),
-    create_machines(ServerList, 1, Machines1, Machines2, Machines3, Machines4),
+    create_machines(ServerList, 1, CoresList, FreqList, Machines1, Machines2, Machines3, Machines4),
     /*
     write('----- Machines 1 ----- '), nl,
     print_list(Machines1), nl,
@@ -66,6 +49,23 @@ schedule(ServerList, ServerNo, TaskList, TaskNo, StartTimes, EndTimes, MachineId
     print_list(Machines3), nl,
     write('----- Machines 4 ----- '), nl,
     print_list(Machines4), nl, */
+    %Tasks
+    length(MachineIds, TaskNo),
+    domain(MachineIds, 1, ServerNo),
+    length(Tasks1, TaskNo),
+    length(Tasks2, TaskNo),
+    length(Tasks3, TaskNo),
+    length(Tasks4, TaskNo),
+    create_prolog_tasks(TaskList, StartTimes, EndTimes, MachineIds, CoresList, FreqList, Tasks1, Tasks2, Tasks3, Tasks4),
+    /*
+    write('----- Tasks 1 ----- '), nl,
+    print_list(Tasks1), nl,
+    write('----- Tasks 2 ----- '), nl,
+    print_list(Tasks2), nl,
+    write('----- Tasks 3 ----- '), nl,
+    print_list(Tasks3), nl,
+    write('----- Tasks 4 ----- '), nl,
+    print_list(Tasks4), nl, */
     cumulatives(Tasks1, Machines1, [bound(upper)]),
     cumulatives(Tasks2, Machines2, [bound(upper)]),
     cumulatives(Tasks3, Machines3, [bound(upper)]),
@@ -76,26 +76,35 @@ schedule(ServerList, ServerNo, TaskList, TaskNo, StartTimes, EndTimes, MachineId
     append([MachineIds, StartTimes], Vars),
     labeling([minimize(End)], Vars). %Minimizar simetrico de plano de tarefas (?)
 
-create_machines([], _, [], [], [], []).
-create_machines([Server | Rsl], MachineId, [M1 | Rm1], [M2 | Rm2], [M3 | Rm3], [M4 | Rm4]) :-
+create_machines([], _, [], [], [], [], [], []).
+create_machines([Server | Rsl], MachineId, [NoCores | Rc], [Frequency | Rf], [M1 | Rm1], [M2 | Rm2], [M3 | Rm3], 
+    [M4 | Rm4]) :-
     Server = [NoCores, Frequency, RAM, Storage],
     M1 = machine(MachineId, NoCores),
     M2 = machine(MachineId, Frequency),
     M3 = machine(MachineId, RAM),
     M4 = machine(MachineId, Storage),
     NextMachineId is (MachineId + 1),
-    create_machines(Rsl, NextMachineId, Rm1, Rm2, Rm3, Rm4).
+    create_machines(Rsl, NextMachineId, Rc, Rf, Rm1, Rm2, Rm3, Rm4).
 
-create_prolog_tasks([], [], [], [], [], [], [], []).
-create_prolog_tasks([UserTask | Rtl], [StartTime | Rs], [EndTime | Re], [MachineId| Rmid], 
+create_prolog_tasks([], [], [], [], _, _, [], [], [], []).
+create_prolog_tasks([UserTask | Rtl], [StartTime | Rs], [EndTime | Re], [MachineId| Rmid], CoresList, FreqList,
     [Task1 | Rt1], [Task2 | Rt2], [Task3 | Rt3], [Task4 | Rt4]) :-
-    UserTask = [_, NoCores, Frequency, RAM, Storage, Duration],
+    UserTask = [_, NoCores, Frequency, RAM, Storage, ETA],
+    TaskCost is (NoCores * Frequency),
+    element(MachineId, CoresList, CoreS),
+    element(MachineId, FreqList, FreqS),
+    CPUSpeed #= (CoreS * FreqS),
+    Diff #= (CPUSpeed - TaskCost),
+    HalfwayPoint #= (CPUSpeed / 2),
+    (Diff #>= HalfwayPoint #/\ Duration #= (ETA / (Diff / TaskCost))) 
+    #\/ (Diff #< HalfwayPoint #/\ Duration #= (ETA * (TaskCost / Diff))),
     %Check For Duration Discrepancies (h vs mins)
     Task1 = task(StartTime, Duration, EndTime, NoCores, MachineId),
     Task2 = task(StartTime, Duration, EndTime, Frequency, MachineId),
     Task3 = task(StartTime, Duration, EndTime, RAM, MachineId),
-    Task4 = task(StartTime, Duration, EndTime, Storage, MachineId), %This vs having [Mid1, Mid2, Mid3, Mid4] and restricting them to be equal to each other
-    create_prolog_tasks(Rtl, Rs, Re, Rmid, Rt1, Rt2, Rt3, Rt4).
+    Task4 = task(StartTime, Duration, EndTime, Storage, MachineId), 
+    create_prolog_tasks(Rtl, Rs, Re, Rmid, CoresList, FreqList, Rt1, Rt2, Rt3, Rt4).
     
 generate_data(ServerList, NoServers, TaskList, TaskNo) :-
     write('Server Amount: '),
